357ce3bd6d3201aebe3b48624743dec769624145
luuxis <61792917+luuxis@users.noreply.github.com>
2020-09-06 17:11:34 +0200

    1.2.1

--- app/assets/js/scripts/settings.js
+++ app/assets/js/scripts/settings.js
@@ -1284,46 +1284,14 @@ function settingsUpdateButtonStatus(text, disabled = false, handler = null){
  * 
  * @param {Object} data The update data.
  */
-function populateSettingsUpdateInformation(data){
-    if(data != null){
-        settingsUpdateTitle.innerHTML = `Nouvelle version de launcher disponible.`
-        settingsUpdateChangelogCont.style.display = null
-        settingsUpdateChangelogTitle.innerHTML = data.releaseName
-        settingsUpdateChangelogText.innerHTML = data.releaseNotes
-        populateVersionInformation(data.version, settingsUpdateVersionValue, settingsUpdateVersionTitle, settingsUpdateVersionCheck)
-        
-        if(process.platform === 'darwin'){
-            settingsUpdateButtonStatus('Download from GitHub<span style="font-size: 10px;color: gray;text-shadow: none !important;">Close the launcher and run the dmg to update.</span>', false, () => {
-                shell.openExternal(data.darwindownload)
-            })
-        } else {
-            settingsUpdateButtonStatus('Téléchargement de la nouvelle version du launcher..', true)
-        }
-    } else {
-        settingsUpdateTitle.innerHTML = 'Dernière version du launcher installer.'
-        settingsUpdateChangelogCont.style.display = 'none'
-        populateVersionInformation(remote.app.getVersion(), settingsUpdateVersionValue, settingsUpdateVersionTitle, settingsUpdateVersionCheck)
-        settingsUpdateButtonStatus('Rechercher une mise à jour.', false, () => {
-            if(!isDev){
-                ipcRenderer.send('autoUpdateAction', 'checkForUpdate')
-                settingsUpdateButtonStatus('Rechercher une mise à jour.', true)
-            }
-        })
-    }
-}
+
 
 /**
  * Prepare update tab for display.
  * 
  * @param {Object} data The update data.
  */
-function prepareUpdateTab(data = null){
-    populateSettingsUpdateInformation(data)
-}
 
-/**
- * Settings preparation functions.
- */
 
 /**
   * Prepare the entire settings UI.
@@ -1334,7 +1302,6 @@ function prepareSettings(first = false) {
     if(first){
         setupSettingsTabs()
         initSettingsValidators()
-        prepareUpdateTab()
     } else {
         prepareModsTab()
     }
